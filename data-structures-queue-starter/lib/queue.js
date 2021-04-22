89ad8b96608186c5cace48cbda650fa056527829
marvin <marvinma.mw@gmail.com>
2021-04-22 17:23:04 -0400

    question 2 -1

--- data-structures-queue-starter/lib/queue.js
+++ data-structures-queue-starter/lib/queue.js
@@ -13,7 +13,7 @@
 // Constraints:
 // ------------
 //
-// Make sure the time and space complexity of each is equivalent to those 
+// Make sure the time and space complexity of each is equivalent to those
 // in the table provided in the Time and Space Complexity Analysis section
 // of your Stack reading!
 //
@@ -21,11 +21,45 @@
 // Let's Code!
 // -----------
 
+
+///FIFO
+
 class Node {
+    constructor(value) {
+        this.value=value;
+        this.next=null;
+    }
 
 }
 
 class Queue {
+    constructor(){
+        this.front=null;
+        this.back=null;
+        this.length=0;
+    }
+
+    enqueue(value){
+        let newNode=new Node(value);
+        if(!this.length){
+            this.front=newNode;
+        }else{
+            newNode.next=this.front;
+            this.front=this.back;
+            this.back=newNode.next;
+        }
+        return ++this.length;
+    };
+
+
+    dequeue(){
+
+    };
+
+
+    size(){
+
+    };
 
 }
 
