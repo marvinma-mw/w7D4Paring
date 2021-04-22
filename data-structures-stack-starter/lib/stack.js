535d960ac1b84e90c89065508e01304f8f518599
tkenned2020 <tkenned2020@outlook.com>
2021-04-22 17:27:59 -0400

    pop function complete

--- data-structures-stack-starter/lib/stack.js
+++ data-structures-stack-starter/lib/stack.js
@@ -22,35 +22,52 @@
 // -----------
 
 class Node {
-    constructor(value){
-        this.value=value;
-        this.next=null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class Stack {
-    constructor(){
-        this.top = null;
-        this.length = 0;
-        this.last=null;
+  constructor() {
+    this.top = null;
+    this.length = 0;
+    this.last = null;
+  }
+
+  push(value) {
+    let newNode = new Node(value);
+    if (!this.length) {
+      this.top = newNode;
+    } else {
+      this.last = this.top;
+      this.top = newNode;
+    }
+    return ++this.length;
+  }
+
+  pop() {
+    if (!this.length) {
+      return null;
     }
 
-    push(value){
-        let newNode=new Node(value);
-        if(!this.length){
-            this.top=newNode;
-            // this.length++;
-        }else{
-            this.last=this.top;
-            this.top=newNode;
-            // this.length++;
-        }
-        return ++this.length;
+    const oldTop = this.top;
+
+    if (this.top === this.last) {
+      this.last = null;
     }
+
+    this.top = this.top.next;
+    --this.length;
+    return oldTop.value;
+  }
 }
 
-let stack1= new Stack();
+let stack1 = new Stack();
 console.log(stack1.push(1));
+console.log(stack1.push(2));
+console.log(stack1.push(3));
+console.log(stack1.pop());
 
 
 exports.Node = Node;
